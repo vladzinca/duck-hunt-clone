7f92996806d7a695731efde0d55427eb3c510e85
vladzinca <vlad_zinca@yahoo.com>
2022-11-06 01:03:12 +0200

    Corrected version

---
+++ src/lab_m1/Tema1/obj2D.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+#include "core/gpu/mesh.h"
+#include "utils/glm_utils.h"
+
+
+namespace obj2D
+{
+
+    Mesh* CreateTriangle(const std::string& name, glm::vec3 corner1, glm::vec3 corner2, glm::vec3 corner3, glm::vec3 color, bool fill = false);
+    Mesh* CreateCircle(const std::string& name, glm::vec3 center, float radius, float triangleCount, glm::vec3 color, bool fill);
+    // Create square with given bottom left corner, length and color
+    Mesh* CreateSquare(const std::string &name, glm::vec3 leftBottomCorner, float length, glm::vec3 color, bool fill = false);
+}
